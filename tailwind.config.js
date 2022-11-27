3e7761a3ec55e2db2c5f5def703e487925c9c6ae
Anand Menon <anandsm08@gmail.com>
2022-11-27 22:25:09 +0530

    commit 3.0:Final Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -9,6 +9,17 @@ module.exports = {
     extend: {
       fontFamily: {
         akira: 'Akira'
+      },
+      colors:{
+        'dark1':'#363062',
+        'dark2':'#4D4C7D',
+        'dark3':'#827397',
+        'dark4':'#D8B9C3',
+        'dark5':'#1C0C5B',
+        'dark6':'#3D2C8D',
+        'dark7':'#916BBF',
+        'light':'#BCEAD5',
+
       }
     },
   },
