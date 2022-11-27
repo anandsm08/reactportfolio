4311b67761cfc690f3e6f54fce1f9e3c64967ef1
Anand Menon <anandsm08@gmail.com>
2022-11-27 22:53:31 +0530

    commit 3.0:Final Commit

--- pages/index.js
+++ pages/index.js
@@ -33,15 +33,17 @@ export default function Home() {
             <h1 className=" text-lg ml-20 font-akira ">AM</h1>
             <ul className="  flex  mr-5 items-center ">
               <li className="ml-4   text-md ">
-                
-                <AnchorLink href="#projects" >Work</AnchorLink>
+                <AnchorLink href="#projects">Work</AnchorLink>
               </li>
               <li className=" ml-4 text-md ">
                 <a href="#">Resume</a>
               </li>
 
               <li className="ml-4 mr-4">
-                <BsMoonStarsFill className="cursor-pointer" onClick={()=> setDarkMode(!darkMode)} />
+                <BsMoonStarsFill
+                  className="cursor-pointer"
+                  onClick={() => setDarkMode(!darkMode)}
+                />
               </li>
             </ul>
           </nav>
@@ -86,11 +88,16 @@ export default function Home() {
           </div>
         </section>
 
-        <section class="about-me"className="min-h-screen bg-gradient-to-b from-sky-100 to-sky-200 dark:bg-none">
+        <section
+          class="about-me"
+          className="min-h-screen bg-gradient-to-b from-sky-100 to-sky-200 dark:bg-none"
+        >
           <div className="">
             <div className="flex justify-between">
               <div className="mx-10 justify-center dark:text-dark4">
-                <h2 className="text-4xl font-mono py-10 px-28 dark:text-light">Who am I?</h2>
+                <h2 className="text-4xl font-mono py-10 px-28 dark:text-light">
+                  Who am I?
+                </h2>
                 <p className="text-xl font-medium  px-32">
                   Namaste! Myself Anand Menon{" "}
                 </p>
@@ -98,17 +105,18 @@ export default function Home() {
                   I&apos;ve been learning, tinkering and exploring various
                   fields and would love to help the community by my
                   contributions someday!.Also a HAM Radio Operator.{" "}
-                  </p>
+                </p>
                 <p className="text-xl font-medium ml-32">
                   Working on Flutter/Dart, Kotlin,C/C++. Currently learning
                   Javascript,ReactJS,TailwindCSS to boost my development skills.{" "}
-                
                 </p>
               </div>
             </div>
             <div className=" font-mono py-10 px-28 ml-10 text-gray-800 font-semibold ">
               <h2 className="text-2xl py-8 dark:text-light">When and Where</h2>
-              <h3 className="text-xl underline dark:text-emerald-200 ">2020-2024</h3>
+              <h3 className="text-xl underline dark:text-emerald-200 ">
+                2020-2024
+              </h3>
               <ul className="text-xl ml-10 py-2 list-disc dark:text-dark4">
                 <li>
                   Currently a Student at{" "}
@@ -120,7 +128,9 @@ export default function Home() {
                 </li>
                 <li>Tech at Google Developers Student Clubs,PCE (2022-23). </li>
               </ul>
-              <h3 className="text-xl underline dark:text-emerald-200">2018-2020</h3>
+              <h3 className="text-xl underline dark:text-emerald-200">
+                2018-2020
+              </h3>
               <ul className="text-xl ml-10 py-2 list-disc dark:text-dark4">
                 <li>
                   Studied and completed my HSC from{" "}
@@ -142,7 +152,9 @@ export default function Home() {
           id="projects"
         >
           <div clasName="mx-10 justify-center">
-            <h2 className="text-4xl font-mono py-10 px-28 dark:text-light">Projects</h2>
+            <h2 className="text-4xl font-mono py-10 px-28 dark:text-light">
+              Projects
+            </h2>
           </div>
           <div className="grid xl:grid-cols-4 md:grid-cols-4 sm:grid-cols-1 gap-2 px-12">
             <div className="rounded-lg h-70 w-60 mx-10 dark:text-light">
@@ -162,7 +174,10 @@ export default function Home() {
                   <div class="flex justify-end py-2 ">
                     <div class="my-2">
                       <div class="flex space-x-2 align-middle rounded-lg bg-white mr-4 dark:text-black">
-                      <a href="https://github.com/anandsm08/ExpenseTracker_Expensa"> <AiFillGithub className="h-8 w-8"></AiFillGithub></a>
+                        <a href="https://github.com/anandsm08/ExpenseTracker_Expensa">
+                          {" "}
+                          <AiFillGithub className="h-8 w-8"></AiFillGithub>
+                        </a>
                       </div>
                     </div>
                     <div class="my-2 rounded-lg bg-white">
@@ -182,7 +197,9 @@ export default function Home() {
                   <p class="text-xl font-semibold my-2">Portfolio Website</p>
                   <div class="flex space-x-2 text-black text-sm py-2">
                     <AiFillCode className="h-6 w-6 dark:text-white"></AiFillCode>
-                    <p className="text-lg dark:text-white">ReactJs | TailwindCSS</p>
+                    <p className="text-lg dark:text-white">
+                      ReactJs | TailwindCSS
+                    </p>
                   </div>
 
                   <div class="border-t-2 "></div>
@@ -190,11 +207,17 @@ export default function Home() {
                   <div class="flex justify-end py-2 ">
                     <div class="my-2">
                       <div class="flex space-x-2 align-middle rounded-lg bg-white mr-4 dark:text-black">
-                      <a href="https://github.com/anandsm08/ExpenseTracker_Expensa"> <AiFillGithub className="h-8 w-8"></AiFillGithub></a>
+                        <a href="https://github.com/anandsm08/ExpenseTracker_Expensa">
+                          {" "}
+                          <AiFillGithub className="h-8 w-8"></AiFillGithub>
+                        </a>
                       </div>
                     </div>
                     <div class="my-2 rounded-lg bg-white dark:text-black">
-                     <a href="#"> <FiArrowUpRight className="h-8 w-8"></FiArrowUpRight></a>
+                      <a href="#">
+                        {" "}
+                        <FiArrowUpRight className="h-8 w-8"></FiArrowUpRight>
+                      </a>
                     </div>
                   </div>
                 </div>
@@ -218,7 +241,10 @@ export default function Home() {
                   <div class="flex justify-end py-2 ">
                     <div class="my-2">
                       <div class="flex space-x-2 align-middle rounded-lg bg-white mr-4 dark:text-black">
-                      <a href="https://github.com/anandsm08/ExpenseTracker_Expensa"> <AiFillGithub className="h-8 w-8"></AiFillGithub></a>
+                        <a href="https://github.com/anandsm08/ExpenseTracker_Expensa">
+                          {" "}
+                          <AiFillGithub className="h-8 w-8"></AiFillGithub>
+                        </a>
                       </div>
                     </div>
                     <div class="my-2 rounded-lg bg-white">
@@ -228,10 +254,17 @@ export default function Home() {
                 </div>
               </div>
             </div>
-
           </div>
         </section>
+        <footer className=" text-center  lg:text-left">
+        <div className="text-gray-300 text-center text-lg p-6 bg-gradient-to-b from-sky-600 to-sky-800 dark:bg-none dark:text-light" >
+            © Copyright 2022, Developed by 
+            <a className="text-gray-100 font-semibold dark:text-emerald-300" href="#"> Anand Menon</a>
+        </div>
+      </footer>
       </main>
+      
     </div>
+    
   );
 }
